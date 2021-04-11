c989fb50d78a4f2b7195d68769dae268c63436a6
sam <syed.simanta06@gmail.com>
2021-04-12 00:35:15 +0600

    first commit

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  avatar: {
+    type: String,
+  },
+
+  joinDate: {
+    type: Date,
+    default: Date.now
+  },
+
+  favourites: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: 'Post'
+  }
+
+});
+
+module.exports = mongoose.model("User", UserSchema);
