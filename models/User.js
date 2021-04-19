a42052a4b19526140d176fd61cb7e052acd7a3c1
sam <syed.simanta06@gmail.com>
2021-04-20 00:42:54 +0600

    'final'

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -19,20 +20,40 @@ const UserSchema = new Schema({
     trim: true
   },
   avatar: {
-    type: String,
+    type: String
   },
-
   joinDate: {
     type: Date,
     default: Date.now
   },
-
-  favourites: {
+  favorites: {
     type: [mongoose.Schema.Types.ObjectId],
     required: true,
-    ref: 'Post'
+    ref: "Post"
   }
+});
 
+// Create and add avatar to user
+UserSchema.pre("save", function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+// Hash password so it can't be seen w/ access to database
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
